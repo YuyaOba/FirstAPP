fdd4476f361dd568ec2dba2892982a1c1aa15be7
Yuya Oba <yuyaoba.0501@gmail.com>
2020-11-10 16:49:59 +0900

    Cを押した際にHeaderの表記を0にする

---
+++ FirstApp/ Decision.swift
@@ -0,0 +1,16 @@
+//
+//  DecisionButton.swift
+//  FirstApp
+//
+//  Created by 大場勇弥 on 2020/11/10.
+//  Copyright © 2020 大場勇弥. All rights reserved.
+//
+
+import UIKit
+
+class DecisionButton: UIButton {
+    
+    let decisionButton = UIButton()
+    
+    
+}
